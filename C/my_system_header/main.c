2977e921912f8d9c954806c93267f5cc87670d49
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-02-14 12:41:50 +0100

    feat(C): Act like a system header

---
+++ C/my_system_header/main.c
@@ -0,0 +1,8 @@
+// Compile with "-isystem ."
+#include <my_header.h> // Notice the angled brackets
+#include <stdio.h>
+
+int main(void)
+{
+	printf(DUCK);
+}
---
+++ C/my_system_header/my_header.h
@@ -0,0 +1,3 @@
+#pragma system_header // Not needed, but grants some immunity against a few warnings
+
+#define DUCK "I am a duck.\n"
